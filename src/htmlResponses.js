b9be40341ca96da1d65a97c95603378782c01250
rbs5340 <rbs5340@g.rit.edu>
2025-01-27 01:15:51 -0500

    Finished Simple HTTP

---
+++ src/htmlResponses.js
@@ -0,0 +1,27 @@
+const fs = require('fs'); // pull in the file system module
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const meme = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const getPage2 = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page2);
+  response.end();
+};
+
+const getMeme = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(meme);
+  response.end();
+};
+
+module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
+module.exports.getMeme = getMeme;
